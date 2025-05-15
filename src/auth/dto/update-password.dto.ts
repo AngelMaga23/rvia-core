e2c8d632b08239a41ee99e4166f3c332635761aa
AngelMaga23 <angelmaga23@outlook.com>
2025-05-15 17:08:17 -0600

    Contrasena cambio

--- src/auth/dto/update-password.dto.ts
+++ src/auth/dto/update-password.dto.ts
@@ -1,13 +1,15 @@
 import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { CreateUserDto } from './create-user.dto';
+import { PickType } from "@nestjs/mapped-types";
 
-export class UpdatePasswordDto {
+export class UpdatePasswordDto extends PickType(CreateUserDto, ['nom_contrasena'] as const){
 
-    @IsString()
-    @MinLength(6)
-    @MaxLength(50)
-    @Matches(/^(?=.*[A-ZÑ])(?=.*\d)(?=.*[@$!%*?&#.])[A-Za-zñÑ\d@$!%*?&#.]{12,}$/, {
-      message: 'La contraseña debe tener al menos una letra mayúscula, un número y un carácter especial.'
-    })
-    nom_contrasena: string;
+    // @IsString()
+    // @MinLength(6)
+    // @MaxLength(50)
+    // @Matches(/^(?=.*[A-ZÑ])(?=.*\d)(?=.*[@$!%*?&#.])[A-Za-zñÑ\d@$!%*?&#.]{12,}$/, {
+    //   message: 'La contraseña debe tener al menos una letra mayúscula, un número y un carácter especial.'
+    // })
+    // nom_contrasena: string;
 
 }
