26ef99bb69bca73255cdcbc9ccd9a3b0ec0f9f1f
AngelMaga23 <angelmaga23@outlook.com>
2025-05-15 16:22:20 -0600

    Contrasena cambio

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -37,7 +37,7 @@ export class CreateUserDto {
     @IsString()
     @MinLength(6)
     @MaxLength(50)
-    @Matches(/^(?=.[A-ZÑ])(?=.\d)(?=.[@$!%?&#.])[A-Za-zñÑ\d@$!%*?&#.]{12,}$/, {
+    @Matches(/^(?=.*[A-ZÑ])(?=.*\d)(?=.*[@$!%*?&#.])[A-Za-zñÑ\d@$!%*?&#.]{12,}$/, {
       message: 'La contraseña debe tener al menos una letra mayúscula, un número y un carácter especial.'
     })
     nom_contrasena: string;
